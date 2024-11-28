cb227ae18fa9987f46fd3663d27e520568303741
munna5959 <161248279+munna5959@users.noreply.github.com>
2024-11-28 22:58:42 +0600

    DAY-28

---
+++ Day-28 Js-Datatypes And Variable/datatypes.js
@@ -0,0 +1,20 @@
+
+let age = 25;  // for Intiger number
+let price = 19.99; //for foating number
+
+
+let name = "John Doe";
+let greeting = 'Hello, World!'; // this is string
+
+
+let isStudent = true;
+let hasGraduated = false; //for boolean numbers
+
+
+let bigNumber = BigInt(123456789012345678901234567890); //for big numbers
+console.log(bigNumber);
+
+console.log("Number:", age, price);
+console.log("String:", name, greeting);
+console.log("Boolean:", isStudent, hasGraduated);
+console.log("BigInt:", bigNumber);
