c2f0c4fd1e51ba7bcdd08ae1392022819227a30c
munna5959 <161248279+munna5959@users.noreply.github.com>
2024-12-05 16:03:10 +0600

    DAY-35

---
+++ Day-35 js-Math/app.js
@@ -0,0 +1,16 @@
+let num=20.490000909
+
+//Normal Number Print
+console.log(num);
+
+//Highest Number Print
+console.log(Math.ceil(num));
+//Round Number Print
+console.log(Math.round(num));
+//Lowest Number Print
+console.log(Math.floor(num));
+//Square Value Print
+console.log(Math.pow(3,2));
+
+//Generation Random NUmbers
+console.log(Math.round((Math.random().toFixed(2)*10)));
