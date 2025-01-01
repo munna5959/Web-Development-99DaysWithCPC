53dcd281c9d04bd84f8722698c1560ae564385ea
Md. Munna Hosain <161248279+munna5959@users.noreply.github.com>
2025-01-01 22:53:02 +0600

    DAY-62

---
+++ Day-62 React Functional Componat/src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About= () =>{
+  return (
+    <div>
+      <h2>This is Munna Hosain</h2>
+      <p>From , Dhaka International University</p>
+    </div>
+  )
+}
+
+export default About
---
+++ Day-62 React Functional Componat/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from'./Home'
+import About from './About'
+
+function App() {
+
+  return (
+    <>
+      <Home></Home>
+      <About></About>
+        
+    </>
+  )
+}
+
+export default App
