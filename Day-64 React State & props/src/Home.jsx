239b7ccb6df5bba07e4635c9300d6a5b677d91ff
Md. Munna Hosain <161248279+munna5959@users.noreply.github.com>
2025-01-03 18:11:57 +0600

    DAY-64

---
+++ Day-64 React State & props/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Services from "./Services"
+
+function Home(){
+  return(
+    <>
+      <h3>This is munna</h3>
+      <Services name="Munna Hosain" age="21"></Services>
+      <Services name="Omar Faruq" age="23"></Services>
+      <Services name="Muhtasim Fuad" age="22"></Services>
+      <Services name="Humayra Kabir" age="19"></Services>
+    </>
+  )
+}
+
+export default Home;
---
+++ Day-64 React State & props/src/Services.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import  "./Services.css"
+
+function Services(myprops){
+  return(
+    <h2>My name is {myprops.name} and age is {myprops.age}</h2>
+  )
+}
+
+export default Services;
